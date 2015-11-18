f8ad593982599ae98deddfa950dedd14fb9c1171
ptwickler <peter.twickler@gmail.com>
2015-11-18 16:19:50 -0500

    fixed click handler for connecting objects with wires. Now works, but gates don't update dynamically, but wires do?

--- gate.js
+++ gate.js
@@ -1,108 +1,70 @@
-var And = function(x,y,r,state,id){                              // This is the object constructor for the AND gate. i
+function Gate(x,y,state,id){
     this.x = x;
-    this.y =y;
-    this.r = r;
-    this.fill = "red";
-    this.id = id;
-    this.instate1 ;
-    this.instate2  ;
-    this.out_state = 0;
+    this.y = y;
+    this.r = 20;
+    this.input01 = [];
+    this.input02 = [];
+    this.output = [];
     this.state = state;
-    // this.pos = col.length;
-    this.inputs = [];
-    this.outputs = [];
-    this.body ="" ;
+    this.id = id;
+    this.fill = "";
+    this.body = "";
+    this.out_state = 0;
 
-    this.connect = function(gate, gate_put){   //tells one gate to listen to the output of another
-        gate_put.push(gate);
-        this.bod();
-    };
-
-    this.disconnect = function(gate, gate_put){
-        gate_put.splice(gate_put.indexOf(gate), 1);
-        this.bod();
-    };
-
-    this.recieve = function (state) {
-
-        if (!this.instate1  && !this.instate2){
-            this.instate1 = state;
-        }
-        else if (this.instate1 && !this.instate2){
-            this.instate2 = state;
-        }
-        else{
-
-            if(state == 0){
-                if(this.instate1 ==0 && this.instate2 ==1){
-                    this.instate2 = 0;
-                }
-                else if(this.instate1 ==1 && this.instate2 ==0){
-                    this.instate1 =0;
-                }
-
-                else {
-                    this.instate1 =0;
-                }
-            }
-
-            if (state ==1){
-                if(this.instate1 ==0 && this.instate2 ==1){
-                    this.instate1 =1;
-                }
-                else if (this.instate1 ==1 && this.instate2 ==0){
-                    this.instate2 = 1;
-                }
-
-            }
-        }
-
-        this.bod();
-        var sts = this.state;
-        this.transmit(sts);
-    };
-
-    /* this.recieve = function (state) {
-     this.state = state;
-     this.bod();
-     this.transmit(state);
-     };*/
-    this.transmit= function(state){
-        for (var i=0; i<this.outputs.length;i++) {
-            this.outputs[i].recieve(state);
-            this.bod();
-        }
-    };
-
-
-    this.bod = function () {                       //this is the method that creates the body of the circle object.
-        if (this.instate1 ==1 && this.instate2 ==1){
-            this.state =1;
-        }
-
-        else {
-            this.state =0;
-        }
-        if (this.state == 0){                      // this if statement sets fill to blue if the state property is "0"
-            this.fill = "blue";
-            this.out_state = 0;
-        }
-        if (this.state==1){                       // this if statement sets the fill to red if the state property is 1
-            this.fill="red";
-            this.out_state = 1;
-        }
-
-        this.body = "<circle cx=\"" + this.x + "\" cy=\"" + this.y + "\" r=\"" + this.r + "\" fill=\"" +  this.fill + "\" id=\"" + this.id + "\" class = \"put\">AND</circle>";
-
-        tell();
-
-
-        //control(this); /// instead of pushing the gate to the col[], somehow alert the control that it needs to cycle through the objects and replace each body with the new body.
-    };
 
     this.make = function(){
         this.bod()
         col.push(this);
+
         tell();
     };
+
+    // This connects the gate together. It pushes the output gate into the input array of the input gate. It first
+    // checks to see
+    this.connect = function(gate_out, gate_in){
+        if (gate_in.input01[0]){
+            gate_in.input02[0] = gate_out;
+        }
+
+        else if(gate_in.input02[0]){
+            gate_in.input01[0] = gate_out;
+        }
+
+        else if(gate_in.input01[0] && gate_in.input02[0]){
+            console.log('full');
+        }
+
+
+        else {
+            gate_in.input01[0] = gate_out;
+        }
+        tell();
+    };
+
+
+    this.bod = function(){
+
+        if ((this.input01[0] && this.input01[0].out_state ==1)  && (this.input02[0] && this.input02[0].out_state == 1)){
+            this.state = 1;
+            this.out_state = 1;
+        }
+
+        else {
+            this.state = 0;
+        }
+
+        if (this.state == 0){
+            this.fill = 'blue';
+
+        }
+
+        if (this.state == 1){
+            this.fill = 'red';
+        }
+
+        this.body = '<circle id = "' + this.id + '" cx = "' + this.x + '" cy = "' + this.y + '" r = "' + this.r + '" fill = "' + this.fill + '" />';
+
+
+    }
+
 }
